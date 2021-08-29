618570b4ba8d694a534f05262f61f55181465223
xw6666 <1481016131@qq.com>
2021-08-29 22:14:52 +0800

    增加vector类，更新string类

---
+++ Vector/test.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+//int main()
+//{
+//	//vector数据访问
+//	vector<int> v1;
+//	for (int i = 0; i < 10; i++)
+//	{
+//		v1.push_back(i + 1);
+//	}
+//
+//	//通过[]
+//	for (int i = 0; i < v1.size(); i++)
+//	{
+//		cout << v1[i] << " ";
+//	}
+//	cout << endl;
+//
+//	//通过at
+//	for (int i = 0; i < v1.size(); i++)
+//	{
+//		cout << v1.at(i) << " ";
+//	}
+//	cout << endl;
+//
+//	//front 和 back
+//	cout << v1.front() << endl;
+//	cout << v1.back() << endl;
+//
+//	return 0;
+//}
