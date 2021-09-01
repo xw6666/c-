c377790997037209805c5a7dbb0d5b0281697c4f
xw6666 <1481016131@qq.com>
2021-09-01 23:28:40 +0800

    更新stack容器的使用

--- stack/test.cpp
+++ stack/test.cpp
@@ -1,29 +1,31 @@
 #include <iostream>
 using namespace std;
 #include <stack>
+#include <fstream>
+#include <cctype>
+//int main()
+//{
+//	stack<int> st;
+//	st.push(1);
+//	st.push(2);
+//	st.push(3);
+//	st.push(4);
+//	st.push(5);
+//
+//	//·µ»ØÕ»¶¥ÔªËØ
+//	cout << st.top() << endl;
+//
+//	//É¾³ıÔªËØ
+//	st.pop();
+//
+//	//±éÀústack
+//	while (!st.empty())
+//	{
+//		cout << st.top() << endl;
+//		st.pop();
+//	}
+//
+//	system("pause");
+//	return 0;
+//}
 
-int main()
-{
-	stack<int> st;
-	st.push(1);
-	st.push(2);
-	st.push(3);
-	st.push(4);
-	st.push(5);
-
-	//·µ»ØÕ»¶¥ÔªËØ
-	cout << st.top() << endl;
-
-	//É¾³ıÔªËØ
-	st.pop();
-
-	//±éÀústack
-	while (!st.empty())
-	{
-		cout << st.top() << endl;
-		st.pop();
-	}
-
-	system("pause");
-	return 0;
-}
