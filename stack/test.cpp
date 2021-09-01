0571d9d2942318243b6f4cf97693ddc0f8406c84
xw6666 <1481016131@qq.com>
2021-09-01 11:12:10 +0800

    更新stack容器的用法

---
+++ stack/test.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+#include <stack>
+
+int main()
+{
+	stack<int> st;
+	st.push(1);
+	st.push(2);
+	st.push(3);
+	st.push(4);
+	st.push(5);
+
+	//·µ»ØÕ»¶¥ÔªËØ
+	cout << st.top() << endl;
+
+	//É¾³ıÔªËØ
+	st.pop();
+
+	//±éÀústack
+	while (!st.empty())
+	{
+		cout << st.top() << endl;
+		st.pop();
+	}
+
+	system("pause");
+	return 0;
+}
